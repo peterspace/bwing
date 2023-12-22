9a921288995e2c0c3fc16143c1a6f71b9a356cd5
peterspace <peter.space.io@gmail.com>
2023-12-23 01:28:23 +0300

    thirteen commit

--- client/dist/assets/exchange-b051f109.js
+++
@@ -1,1 +0,0 @@
-import"./redux-toolkit.esm-c1cf81d4.js";import"./buyCard-77c6b69e.js";
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,28 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-// import { Auth } from './src/pages/Auth/Auth';
-// import { Dashboard } from './src/pages/Dashboard/Dashboard';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        // Assuming these are your entry files for different pages
-        app: './src/App',
-        auth: './src/pages/Auth/Auth',
-        dashboard: './src/pages/Dashboard/Dashboard',
-        buyCard: './src/pages/BuyCard/BuyCard',
-        buyCash: './src/pages/BuyCash/BuyCash',
-        sellCard: './src/pages/SellCard/SellCard',
-        sellCash: './src/pages/SellCash/SellCash',
-        defi: './src/pages/Defi/Defi',
-        exchange: './src/pages/Exchange/Exchange',
-        // Add more entries as needed
-      },
-    },
-  },
-});
+})
